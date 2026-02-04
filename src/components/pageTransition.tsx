70e35c9bf701d2317d7749058d7ef82a00d82a6d
jordan-furr <jordanfurr98@gmail.com>
2026-02-03 19:40:48 -0500

    recipe size

--- src/components/pageTransition.tsx
+++ src/components/pageTransition.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { motion, AnimatePresence } from 'framer-motion'
+import { motion } from 'framer-motion'
 import { ReactNode } from 'react'
 import { usePathname } from 'next/navigation'
 
@@ -8,16 +8,13 @@ export default function PageTransition({ children }: { children: ReactNode }) {
   const pathname = usePathname()
   
   return (
-    <AnimatePresence mode="wait">
-      <motion.div
-        key={pathname}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: 0.22 }}
-      >
-        {children}
-      </motion.div>
-    </AnimatePresence>
+    <motion.div
+      key={pathname}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.22 }}
+    >
+      {children}
+    </motion.div>
   )
 }
