97a206e0eba93eb2be3d2a7fc2e01c43c0fc2f0b
jordan-furr <jordanfurr98@gmail.com>
2026-02-03 19:35:40 -0500

    transition mobile

--- src/components/pageTransition.tsx
+++ src/components/pageTransition.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { motion } from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion'
 import { ReactNode } from 'react'
 import { usePathname } from 'next/navigation'
 
@@ -8,13 +8,16 @@ export default function PageTransition({ children }: { children: ReactNode }) {
   const pathname = usePathname()
   
   return (
-    <motion.div
-      key={pathname}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.22 }}
-    >
-      {children}
-    </motion.div>
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={pathname}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.22 }}
+      >
+        {children}
+      </motion.div>
+    </AnimatePresence>
   )
 }
